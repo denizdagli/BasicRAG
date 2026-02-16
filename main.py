ee8e38afd7ce63e1c7bffff0e5bcc9d5e846b7a8
denizdagli <denizdaglice@gmail.com>
2026-02-16 15:44:12 +0300

    feat: initial setup for RAG with WebBaseLoader

---
+++ main.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+from langchain import hub
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI
+import bs4
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title","post-header")
+        )
+    )
+)
+
+docs= loader.load()
+
+
+
+if __name__ == "__main__":
+    
+    print(f"Number of documents loaded: {docs}")
